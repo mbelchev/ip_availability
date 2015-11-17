67473f36ee6b4c2276318ffc9a78b7ecb68efac6
mbelchev <marianbelchev@gmail.com>
2015-11-17 03:58:25 +0200

    Editing main method in Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static final Integer SERVER_PORT = 3111;
+	
+	public static void main(String[] args) throws IOException {
+		final Server server = new Server(SERVER_PORT);
+		server.serverStart();
 	}
 
 }
