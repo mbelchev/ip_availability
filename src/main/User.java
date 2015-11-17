056d8ea0a26d894d7d2850b1acc8d84a09c6a2cd
mbelchev <marianbelchev@gmail.com>
2015-11-17 05:35:47 +0200

    Updating user class

--- src/main/User.java
+++ src/main/User.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class User {
 	
@@ -8,7 +11,7 @@ public class User {
 	private boolean isLogged;
 	private Integer loginCounter;
 	private Socket usingSocket;
-	//ToDo: add List<Interval> ...
+	private List<Interval> loginInterval = new ArrayList<Interval>();
 	
 	public User(String username, Socket socket) {
 		this.username 		= username;
@@ -36,11 +39,43 @@ public class User {
 	
 	
 	// Login and Logout
-	public void login() {
-		// ToDo
+	public String login() {
+		if(this.getIsLogged()) return "error: Already logged in";
+		
+		this.isLogged = true;
+		this.loginCounter++;
+		this.loginInterval.add(new Interval(new Date()));
+		
+		return "ok";
 	}
 	
-	public void logout() {
-		// ToDo
+	public String logout() {
+		if(!this.getIsLogged()) return "error: Already logged out";
+		
+		this.isLogged = false;
+		this.loginInterval.get(this.loginInterval.size() - 1).setOut(new Date());
+		
+		return "ok";
+	}
+	
+	// Info
+	public String getInfo() {
+		StringBuilder info = new StringBuilder();
+		
+		info.append(
+				"ok:" 
+				+ this.getUsername()
+				+ ":"
+				+ this.getIsLogged()
+				+ ":"
+				+ this.getLoginCounter()
+		);
+		
+		for (Interval temp_list: this.loginInterval) {
+			info.append(":" + temp_list.from());
+			if (temp_list.to() != null) info.append(":" + temp_list.to());
+		}
+		
+		return info.toString();
 	}
 }
