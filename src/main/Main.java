6e96f7193a586982b62e1912f91650b17f6eb28f
mbelchev <marianbelchev@gmail.com>
2015-11-17 05:02:40 +0200

    opps! wrong method

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class Main {
 
-	private static final Integer SERVER_PORT = 3111;
+	private static final Integer SERVER_PORT = 4112;
 	
 	public static void main(String[] args) throws IOException {
 		final Server server = new Server(SERVER_PORT);
-		server.serverStart();
+		server.startServer();
 	}
 
 }
